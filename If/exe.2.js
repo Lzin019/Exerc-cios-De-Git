3708cca5dc9b6a18278101d383fc22350419e50c
Luiz Felipe <luizfelipeallagedd@gmail.com>
2025-04-04 11:07:33 -0300

    feat(if-else): adiciona comparação do maior entre dois números

---
+++ If/exe.2.js
@@ -0,0 +1,6 @@
+let numero1 = 100
+let numero2 = 20
+if (numero1 > numero2) {
+    console.log("o numero1 é maior que o numero2 ") }
+     else if (numero2 > numero1){console.log('o numero2 e maior que o numero1')}
+  
