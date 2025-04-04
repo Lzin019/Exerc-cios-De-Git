9b5311b7b15417956926169be43225bf24505104
Luiz Felipe <luiz.felipe14@aluno.senai.br>
2025-04-04 16:20:29 -0300

    feat(if-else): adiciona identificação do menor entre três números

---
+++ If/exe.8.js
@@ -0,0 +1,10 @@
+let numero1 = 100
+let numero2 = 206
+let numero3 = 30
+if (numero1 < numero2 && numero1 < numero3) {
+    console.log("o numero1 é menor que o numero2 e o numero3 ") }
+    else if (numero2 < numero3 && numero2 < numero1){
+        console.log("o numero2 é menor que o numero3 e o numero1 ") }
+        else {
+        console.log("o numero3 é menor que o numero2 e o numero1 ") }
+
