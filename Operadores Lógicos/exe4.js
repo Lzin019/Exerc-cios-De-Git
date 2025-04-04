a900c6ca020c5dd1807b63418db5f618210e63e4
Luiz Felipe <luizfelipeallagedd@gmail.com>
2025-04-04 08:38:16 -0300

    feat(operadores-logicos): adiciona operação AND com booleanos

---
+++ Operadores Lógicos/exe4.js
@@ -0,0 +1,5 @@
+let numero1 = 200
+let numero2 = 244
+if(numero1 > 0 && numero2 > 0)
+    {console.log("Sim os dois números são positivos");}
+else{console.log("Não os dois números não são positivos")};
