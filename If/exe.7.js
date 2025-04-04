a2c9942e0ee8a7586e1e8c5f2ccd0a83ada2748e
Luiz Felipe <luiz.felipe14@aluno.senai.br>
2025-04-04 16:07:48 -0300

    feat(if-else): adiciona cálculo de desconto baseado em salário

---
+++ If/exe.7.js
@@ -0,0 +1,7 @@
+let salario = 1000
+
+if (salario >= 2000) {
+    console.log(`O salário após o desconto é de ${salario * 0.9}.`);
+} else {
+    console.log(`O salário após o desconto é de ${salario * 0.95}.`);
+}
