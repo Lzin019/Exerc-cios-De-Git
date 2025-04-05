71de76fc713cb8a5b104f7cc0a827cf1f3a893e5
Luiz Felipe Allage Dionysio <luizfelipeallagedd@gmail.com>
2025-04-04 21:11:50 -0300

    Delete

--- exe.9.js
+++
@@ -1,9 +0,0 @@
-let clima = 11
-if (clima < 15) { console.log("Esta um clima frio🍧") }
-else if (clima >= 15 && clima <= 25) {
-    console.log("Esta um clima agradável👌")
-}
-else {
-    clima > 25
-    console.log(" Está um clima quente🥵")
-}
